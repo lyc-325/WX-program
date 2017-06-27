d3b0300b5e7c3fa09ef4dcbe337ebb4733899155
lsa2127291 <704991434@qq.com>
2017-06-27 19:54:33 +0800

    fix

--- utils/wx-pro.js
+++ utils/wx-pro.js
@@ -64,7 +64,6 @@ function promisify() {
       })
     })
   }
-
   wx.pro.request = options => {
     if (options.toast) {
       wx.showToast({
@@ -96,6 +95,67 @@ function promisify() {
       })
     })
 
+  }
+
+  wx.pro.uploadFile = options => {
+    if (options.toast) {
+      wx.showToast({
+        title: options.toast.title || '加载中',
+        icon: 'loading'      
+        })
+    }
+    return new Promise((resolve, reject) => {
+      wx.uploadFile({
+        url: options.url,
+        header: options.header || {},
+        filePath: options.filePath,
+        formData: options.formData,
+        name: options.name,
+        success: res => {
+          if (res.statusCode >= 400) {
+            console.error('wx.upload fail [business]', options, res.statusCode, res.data)
+            reject(res)
+          }
+          else {
+            console.log('wx.upload success', options, res.data)
+            resolve(res.data) // unwrap data
+          }
+        },
+        fail: err => {
+          console.error('wx.upload fail [network]', options, err)
+          reject(err)
+        }
+      })
+    })
+  }
+
+  wx.pro.downloadFile = options => {
+    if (options.toast) {
+      wx.showToast({
+        title: options.toast.title || '加载中',
+        icon: 'loading'
+      })
+    }
+    return new Promise((resolve, reject) => {
+      wx.downloadFile({
+        url: options.url,
+        header: options.header || {},
+        success: res => {
+          if (res.statusCode >= 400) {
+            console.error('wx.dowload fail [business]', options, res.statusCode, res.data)
+            reject(res)
+          }
+          else {
+            console.log('wx.download success', options, res.data)
+            resolve(res.data) // unwrap data
+          }
+        },
+        fail: err => {
+          console.error('wx.download fail [network]', options, err)
+          reject(err)
+        }
+      })
+    })
   }
 }
 
