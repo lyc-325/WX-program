978696382bbff790680418785d232e38d0831dbd
rxc <98323882@qq.com>
2017-07-28 14:01:54 +0800

    样式调整

---
+++ src/apis/mine.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/7/28 0028.
+ */
+import jf from '../utils/jf'
+
+export const getUserData = function(token,id) {
+  return jf.request({
+    api: `/accounts/detail/${token}/${id}`,
+    data: {},
+    method: 'GET'
+  })
+}
+
