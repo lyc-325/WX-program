813e93c89e6b951e64cb180fdbeafacbd042eca4
liyicheng@rhpass.com <liyicheng@rhpass.com>
2018-09-20 09:58:55 +0800

    登陆权限判断

--- src/config.js
+++ src/config.js
@@ -1,18 +1,9 @@
 module.exports = {
-  // 微信配置
-  wx: {
-    appId: 'wxc7fd029bee462de3',
-    appSecrete: '45d60b1340c220cb760f158dafb4785d'
-  },
-  // 网易云信配置
-  nim: {
-    appKey: 'f1ae99b849c5df091a4e864f0055031f',
-    appSecret: 'bbe69f3eaaab',
-    server: 'https://api.netease.im/nimserver'
-  },
-  // 江风后台配置
-  jf: {
-    server: 'https://service.523dianji.com/api',
-    baseServer: 'https://service.523dianji.com'
+
+  // 后台配置
+  ht: {
+    // server: 'https://rhtapi.rhpass.com'
+    // server: 'http://192.168.0.153:8502',
+    server:'http://dev.rhpass.com:8033',
   }
 }
